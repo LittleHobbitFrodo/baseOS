0ebcbe947bb355c432e3ca64e7ba0ecf8d88d2a1
Samwise Gamgee <sam@gayasshruska.localdomain>
2025-03-28 11:01:24 +0100

    delete unwanted files/directories

--- kernel/rust/bin/debug/build/num-traits-56da0f59bc5e9fc4/build_script_build-56da0f59bc5e9fc4.d
+++
@@ -1,5 +0,0 @@
-/home/sam/Documents/baseOS/kernel/rust/bin/debug/build/num-traits-56da0f59bc5e9fc4/build_script_build-56da0f59bc5e9fc4: /home/sam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/sam/Documents/baseOS/kernel/rust/bin/debug/build/num-traits-56da0f59bc5e9fc4/build_script_build-56da0f59bc5e9fc4.d: /home/sam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/sam/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs:
