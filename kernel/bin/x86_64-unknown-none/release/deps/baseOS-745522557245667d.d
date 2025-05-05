9abcd21fd5598b3ebaca38d22208fbc006ef13bb
Samwise Gamgee <sam@MacBookAir.localdomain>
2025-05-05 13:48:18 +0200

    build works, ready for testing

--- kernel/bin/x86_64-unknown-none/release/deps/baseOS-745522557245667d.d
+++ kernel/bin/x86_64-unknown-none/release/deps/baseOS-745522557245667d.d
@@ -1,6 +1,6 @@
-/Users/sam/docs/prog/baseOS/kernel/bin/x86_64-unknown-none/release/deps/baseOS-745522557245667d: src/prelude.rs src/init.rs src/manage/mod.rs src/manage/kernel_state.rs src/mem/mod.rs
+/Users/sam/docs/prog/BaseOS/rust/kernel/bin/x86_64-unknown-none/release/deps/baseOS-745522557245667d: src/prelude.rs src/init.rs src/manage/mod.rs src/manage/kernel_state.rs src/mem/mod.rs
 
-/Users/sam/docs/prog/baseOS/kernel/bin/x86_64-unknown-none/release/deps/baseOS-745522557245667d.d: src/prelude.rs src/init.rs src/manage/mod.rs src/manage/kernel_state.rs src/mem/mod.rs
+/Users/sam/docs/prog/BaseOS/rust/kernel/bin/x86_64-unknown-none/release/deps/baseOS-745522557245667d.d: src/prelude.rs src/init.rs src/manage/mod.rs src/manage/kernel_state.rs src/mem/mod.rs
 
 src/prelude.rs:
 src/init.rs:
--- kernel/bin/x86_64-unknown-none/release/deps/spin-f5f1ab9177d73667.d
+++ kernel/bin/x86_64-unknown-none/release/deps/spin-f5f1ab9177d73667.d
@@ -1,8 +1,8 @@
-/Users/sam/docs/prog/baseOS/kernel/bin/x86_64-unknown-none/release/deps/libspin-f5f1ab9177d73667.rmeta: lib/spin/src/lib.rs lib/spin/src/barrier.rs lib/spin/src/lazy.rs lib/spin/src/mutex.rs lib/spin/src/mutex/spin.rs lib/spin/src/once.rs lib/spin/src/relax.rs lib/spin/src/rwlock.rs
+/Users/sam/docs/prog/BaseOS/rust/kernel/bin/x86_64-unknown-none/release/deps/libspin-f5f1ab9177d73667.rmeta: lib/spin/src/lib.rs lib/spin/src/barrier.rs lib/spin/src/lazy.rs lib/spin/src/mutex.rs lib/spin/src/mutex/spin.rs lib/spin/src/once.rs lib/spin/src/relax.rs lib/spin/src/rwlock.rs
 
-/Users/sam/docs/prog/baseOS/kernel/bin/x86_64-unknown-none/release/deps/libspin-f5f1ab9177d73667.rlib: lib/spin/src/lib.rs lib/spin/src/barrier.rs lib/spin/src/lazy.rs lib/spin/src/mutex.rs lib/spin/src/mutex/spin.rs lib/spin/src/once.rs lib/spin/src/relax.rs lib/spin/src/rwlock.rs
+/Users/sam/docs/prog/BaseOS/rust/kernel/bin/x86_64-unknown-none/release/deps/libspin-f5f1ab9177d73667.rlib: lib/spin/src/lib.rs lib/spin/src/barrier.rs lib/spin/src/lazy.rs lib/spin/src/mutex.rs lib/spin/src/mutex/spin.rs lib/spin/src/once.rs lib/spin/src/relax.rs lib/spin/src/rwlock.rs
 
-/Users/sam/docs/prog/baseOS/kernel/bin/x86_64-unknown-none/release/deps/spin-f5f1ab9177d73667.d: lib/spin/src/lib.rs lib/spin/src/barrier.rs lib/spin/src/lazy.rs lib/spin/src/mutex.rs lib/spin/src/mutex/spin.rs lib/spin/src/once.rs lib/spin/src/relax.rs lib/spin/src/rwlock.rs
+/Users/sam/docs/prog/BaseOS/rust/kernel/bin/x86_64-unknown-none/release/deps/spin-f5f1ab9177d73667.d: lib/spin/src/lib.rs lib/spin/src/barrier.rs lib/spin/src/lazy.rs lib/spin/src/mutex.rs lib/spin/src/mutex/spin.rs lib/spin/src/once.rs lib/spin/src/relax.rs lib/spin/src/rwlock.rs
 
 lib/spin/src/lib.rs:
 lib/spin/src/barrier.rs:
